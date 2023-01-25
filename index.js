871d8344b0ea1589beed82d65f3eba8d8b779042
Gustavo Ferri Alixandre <gustavo.alixandre@luizalabs.com>
2023-01-24 22:07:35 -0400

    Show 6 first

--- index.js
+++ index.js
@@ -3,26 +3,28 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const PORT = process.env.PORT || 3000;
 
+const PORT = process.env.PORT || 3000;
+const isProd = process.env.NODE_ENV === "production" ? true : false;
 const uri = `mongodb+srv://gustavo:${process.env.DB_PASSWORD}@cluster0.kfflwab.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+const dbName = isProd ? "ranking" : "ranking-dev";
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", async function (req, res) {
   const connection = await client.connect();
-  const db = await connection.db("ranking");
+  const db = await connection.db(dbName);
   const collection = await db.collection("users");
   const data = await collection
     .find({}, { projection: { _id: 0 } })
     .sort({ points: -1 })
-    .limit(5)
+    .limit(6)
     .toArray();
   connection.close();
 
@@ -33,7 +35,7 @@ app.post("/", async (req, res) => {
   const { name, points } = req.body;
 
   const connection = await client.connect();
-  const db = await connection.db("ranking");
+  const db = await connection.db(dbName);
   const collection = await db.collection("users");
   const data = await collection.findOne({ name });
   if (data) {
